a4ecd8e0680d4acbde5429a37af91a5b3377de9d
Dragos-Mihai <dragos_mihai_prodan@yahoo.com>
2019-12-01 20:01:16 +0200

    add rest for ore

--- src/main/java/app/domain/Ora.java
+++ src/main/java/app/domain/Ora.java
@@ -1,9 +1,62 @@
 package app.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
-//@Entity
-//@Table(name = "ore")
-//public class Ora {
-//}
+@Entity
+@Table(name = "ore")
+public class Ora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column
+    private Date data;
+    @Column
+    private Long ora;
+    @Column
+    private Long student;
+
+    public Ora(){}
+    public Ora(Long id, Date data, Long ora){
+        this.id = id;
+        this.data = data;
+        this.ora = ora;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOra() {
+        return ora;
+    }
+
+    public void setOra(Long ora) {
+        this.ora = ora;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Long getStudent() {
+        return student;
+    }
+
+    public void setStudent(Long student) {
+        this.student = student;
+    }
+
+    public String toJson() {
+        return "{ \"id\" : \"" + id + "\", \"data\" : " + data + ", \"ora\" : " + ora + ", \"student\" : " + student + "  }";
+    }
+}
--- src/main/java/app/repo/OraRepo.java
+++ src/main/java/app/repo/OraRepo.java
@@ -1,6 +1,6 @@
 package app.repo;
 
-
+import app.domain.Ora;
 import app.domain.Prezenta;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,8 +8,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "prezente", path = "prezente")
-public interface PrezentaRepo extends PagingAndSortingRepository<Prezenta, Long> {
-
+@RepositoryRestResource(collectionResourceRel = "ore", path = "ore")
+public interface OraRepo extends PagingAndSortingRepository<Ora, Long> {
+    
     List<Prezenta> findByStudent(@Param("student") Long student);
 }
