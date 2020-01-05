1378d9aa5819723c71d090b4eb838a8100912a82
Bejinariu Andrei-Catalin <49789849+ac-bejinariu@users.noreply.github.com>
2020-01-05 20:40:19 +0200

    fixat css pagina ai

--- frontend/src/js/TidorPage.js
+++ frontend/src/js/TidorPage.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 
-import '../css/ai.css'
 import commons from '../css/commons.module.css'
-import ai from '../css/ai.css'
+import ai from '../css/ai.module.css'
 import AI from './AI';
 
 export default function TidorPage() {
@@ -12,24 +11,24 @@ export default function TidorPage() {
     let optionale = ["AI", "Dans", "HCI", "ASG", "PSH", "GHX", "AWE", "CCP"];
 
     return (
-        <div id="ai-content">
-            <div id="ai-left"><br/><br/>
+        <div id={ai.aiContent}>
+            <div id={ai.aiLeft}><br/><br/>
                 <p>Tough to choose an optional course for the final year? Let us recommend you something!</p>
                 <p>What do you like? Write up to three words below</p><br/>
                 <div id="first-form-container">
                     <form id="first-form">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" /><br/><br/>
-                        <button>Submit</button>
+                        <input className={ai.input} type="text" />
+                        <input className={ai.input} type="text" />
+                        <input className={ai.input} type="text" /><br/><br/>
+                        <button className={ai.button}>Submit</button>
                     </form>
                 </div><br/><br/>
 
                 <p>Or... maybe it's easier to tell us what concepts you enjoy, past favourite courses or preferred programming languages in a few sentences:</p>
                 <div id="second-form-container">
                     <form id="second-form">
-                        <textarea id="textarea-input">Write here...</textarea><br/>
-                        <button>Submit</button>
+                        <textarea id="textarea-input" className={ai.textarea}>Write here...</textarea><br/>
+                        <button className={ai.button}>Submit</button>
                     </form><br/><br/>
 
                     <div id="second-form-hidden-content">
@@ -39,16 +38,16 @@ export default function TidorPage() {
                 <div id="any-form-hidden-content">
                     <p>The recommended courses are filtered in the table.</p>
                     <span>If you like what you see let us add them to your profile!</span>
-                    <button>Add</button>
+                    <button className={ai.button}>Add</button>
                 </div>
             </div>
 
-            <div id="ai-right">
+            <div id={ai.aiRight}>
                 <p>{username}</p>
                 <img src={profilePic} />
                 <p>{about}</p>
 
-                <table>
+                <table className={ai.table}>
                     <tbody>
                         <tr>
                             <th>Optionale Sugerate</th>
