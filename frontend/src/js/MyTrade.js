4ecead7e38d349e41efec78a055b39d61b2fd813
Dragos-Mihai <dragos_mihai_prodan@yahoo.com>
2020-01-12 17:55:25 +0200

    Selectare deal-uri functionala -conectare la server pentru ambele screen-uri

--- frontend/src/js/MyTrade.js
+++ frontend/src/js/MyTrade.js
@@ -1,53 +1,6 @@
 import React from 'react'
 import styleCss from '../css/trade.module.css'
 
-class Popup extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			deals: [
-				{id: 1, idD: 1, user: 'user1', ora: '18:00'},
-				{id: 2, idD: 2, user: 'user5', ora: '14:00'},
-				{id: 3, idD: 2, user: 'user3', ora: '8:00'},
-				{id: 4, idD: 2, user: 'user9', ora: '10:00'}
-			]
-		}
-	}
-
-	accepta(id) {
-		alert(id);
-	}
-
-	renderData() {
-		return this.state.deals.map((deal, index) => {
-			const { id, idD, user, ora} = deal
-			return (
-				idD == this.props.idx ? 
-				<div id={ id } className={styleCss.dealMiniStyle}>
-					<h2> {user} </h2>
-					<h2> Ora: {ora} </h2>
-					<button onClick={this.accepta.bind(this, id)}>Accepta</button>
-				</div> : <div></div>
-			)
-		})
-	}
-	
-
-	render() {
-	  return (
-		<div className={styleCss.popup}>
-		  <div className={styleCss.popup_inner}>
-	  	  <h1>{this.props.text}</h1>
-		  <div className={styleCss.scrollerTradeMiniStyle}>
-			{ this.renderData() }
-		  </div>
-		  <button onClick={this.props.closePopup}>Inchide</button>
-		  </div>
-		</div>
-	  );
-	}
-  }
-
 class MyTrade extends React.Component {
 	constructor(props){
 		super(props)
@@ -60,6 +13,11 @@ class MyTrade extends React.Component {
 			],
 			acceptedDeal: [
 				{id: 1, idD: 1, user: 'user1', ora: '18:00'}
+			],
+			toBeAcceptedDeals: [
+				{id: 2, idD: 2, user: 'user5', ora: '14:00'},
+				{id: 3, idD: 2, user: 'user3', ora: '8:00'},
+				{id: 4, idD: 2, user: 'user9', ora: '10:00'}
 			]
 		}
 	}
@@ -87,18 +45,35 @@ class MyTrade extends React.Component {
 		});
 	}
 
+	renderDealData(idK) {
+		return this.state.toBeAcceptedDeals.map((deal, index) => {
+			const { id, idD, user, ora} = deal
+			return (
+				idD == idK ? 
+				<div id={ id } className={styleCss.dealMiniStyle}>
+					<h2> {user} </h2>
+					<h2> Ora: {ora} </h2>
+					<button>Accepta</button>
+				</div> : <div></div>
+			)
+		})
+	}
+
 	renderData() {
 		return this.state.deals.map((deal, index) => {
 			const { id, materie, user1, ora, saptamana, taken } = deal
 			return (
-				<div id={ id } className={styleCss.dealStyle}>
+				<div id={ id } className={taken == false ? (styleCss.dealLargeStyle) : (styleCss.dealStyle)}>
 					<h1> {materie} </h1>
 					<h2> {user1} </h2>
 					<h2> Saptamana {saptamana} </h2>
 					<h2> Ora: {ora} </h2>
 					{
 						taken == false ? (
-							<button onClick={this.togglePopup.bind(this, id)}>Vizualizeaza</button>
+							<div className={styleCss.scrollerTradeBigStyle}>
+								<br></br>
+								{ this.renderDealData(id) }
+							</div>
 						) : (
 							<div>
 								<h3>Deja ai acceptat un schimb.</h3>
@@ -117,7 +92,6 @@ class MyTrade extends React.Component {
 			<div className={styleCss.scrollerTradeStyle}>
 				{ this.renderData() }
 			</div>
-			{this.state.showPopup ?  <Popup text='Vizualizeaza' idx={this.state.selectedID} closePopup={this.togglePopup.bind(this)}/> : null }
 		</div>
         );
     }
