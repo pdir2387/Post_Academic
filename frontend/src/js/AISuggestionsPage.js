b999aa2ecc72cbb1e5a878569f70b3fbb5ff445a
Pricope Stefan-Cristian <pricopestefancristian@gmail.com>
2020-01-17 23:26:26 +0200

    -Added ROSITA™ for the web version ,still needs some design changes but its functional

--- frontend/src/js/AISuggestionsPage.js
+++ frontend/src/js/AISuggestionsPage.js
@@ -1,8 +1,72 @@
 import React, { useState } from 'react'
 
 import commons from '../css/commons.module.css'
-import ai from '../css/ai.module.css'
-import AI from './AI';
+import ai from '../css/ai.module.css';
+import LoadingDots from './LoadingDots'
+
+class RositaTextView extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            logo_rosita: require('../img/logo_rosita.png'),
+            isRositaEnabled: false,
+            index : 0,
+            _interval : -1,
+            colors : ['red', 'blue', 'green', 'black'],
+            phrases : ['Try saying: Who is teaching software security?','Try saying: Search something about artificial intelligence on wikipedia.','Try saying: What\'s the time?','Try saying: What\'s the next big holiday?','Try saying: Where\'s Bucharest?','Try saying: What\'s my schedule for tomorrow?','Try saying: I like databases, what can you recommend?','Try saying: Open youtube.'],
+        }
+    }
+    
+    componentDidMount() {
+        let intervalReference = setInterval(() => {
+            let newIndex = this.state.index + 1;
+
+            this.setState({index : newIndex});
+        }, 2500);
+
+        this.setState({_interval : intervalReference});
+    }
+
+    componentWillUnmount() {
+        if (this.state._interval !== -1)
+            clearInterval(this.state._interval);
+    }
+
+    render() {
+        if (!this.state.isRositaEnabled) {
+            return (
+                <div style={{display: 'flex', flexDirection: "row", padding: 10}}>
+                    <input 
+                        type="checkbox"
+                        onChange={() => this.setState({isRositaEnabled : !this.state.isRositaEnabled})}
+                        checked={this.state.isRositaEnabled}
+                        style={{width: 25, height: 25, marginRight: 30, marginTop: 5}}
+                    />
+                </div>
+            )
+        }
+
+        return (
+            <div style={{display: 'flex', flexDirection: "row", padding: 10}}>
+                <input 
+                    type="checkbox"
+                    onChange={() => this.setState({isRositaEnabled : !this.state.isRositaEnabled})}
+                    checked={this.state.isRositaEnabled}
+                    style={{width: 25, height: 25, marginRight: 30, marginTop: 5}}
+                />
+
+                <img src={this.state.logo_rosita} style={{width: 25, height: 25, marginTop: 5}}/>
+                <div style={{marginTop: 5, elevation: -1}}>
+                    <LoadingDots />
+                </div>
+    
+                <p style={{maxWidth: "80%", fontSize: 16}}>{this.state.phrases[this.state.index % this.state.phrases.length]}</p>    
+            </div>
+        )
+    }
+        
+}
 
 export default function AISuggestionsPage() {
     let [username, setUsername] = useState("Tidor Pricope");
@@ -11,51 +75,56 @@ export default function AISuggestionsPage() {
     let optionale = ["AI", "Dans", "HCI", "ASG", "PSH", "GHX", "AWE", "CCP"];
 
     return (
-        <div id={ai.aiContent}>
-            <div id={ai.aiLeft}><br/><br/>
-                <p>Tough to choose an optional course for the final year? Let us recommend you something!</p>
-                <p>What do you like? Write up to three words below</p><br/>
-                <div id="first-form-container">
-                    <form id="first-form">
-                        <input className={ai.input} type="text" />
-                        <input className={ai.input} type="text" />
-                        <input className={ai.input} type="text" /><br/><br/>
-                        <button className={ai.button}>Submit</button>
-                    </form>
-                </div><br/><br/>
-
-                <p>Or... maybe it's easier to tell us what concepts you enjoy, past favourite courses or preferred programming languages in a few sentences:</p>
-                <div id="second-form-container">
-                    <form id="second-form">
-                        <textarea id="textarea-input" className={ai.textarea}>Write here...</textarea><br/>
-                        <button className={ai.button}>Submit</button>
-                    </form><br/><br/>
-
-                    <div id="second-form-hidden-content">
-                        <p>We identified the following keywords:</p>
-                    </div>
-                </div><br/><br/>
-                <div id="any-form-hidden-content">
-                    <p>The recommended courses are filtered in the table.</p>
-                    <span>If you like what you see let us add them to your profile!</span>
-                    <button className={ai.button}>Add</button>
-                </div>
+        <div id={ai.mainContainer}>
+            <div id={ai.rosita}>
+                <RositaTextView />
             </div>
+            <div id={ai.aiContent}>
+                <div id={ai.aiLeft}><br/><br/>
+                    <p>Tough to choose an optional course for the final year? Let us recommend you something!</p>
+                    <p>What do you like? Write up to three words below</p><br/>
+                    <div id="first-form-container">
+                        <form id="first-form">
+                            <input className={ai.input} type="text" />
+                            <input className={ai.input} type="text" />
+                            <input className={ai.input} type="text" /><br/><br/>
+                            <button className={ai.button}>Submit</button>
+                        </form>
+                    </div><br/><br/>
 
-            <div id={ai.aiRight}>
-                <p>{username}</p>
-                <img src={profilePic} />
-                <p>{about}</p>
+                    <p>Or... maybe it's easier to tell us what concepts you enjoy, past favourite courses or preferred programming languages in a few sentences:</p>
+                    <div id="second-form-container">
+                        <form id="second-form">
+                            <textarea id="textarea-input" className={ai.textarea}>Write here...</textarea><br/>
+                            <button className={ai.button}>Submit</button>
+                        </form><br/><br/>
 
-                <table className={ai.table}>
-                    <tbody>
-                        <tr>
-                            <th>Optionale Sugerate</th>
-                        </tr>
+                        <div id="second-form-hidden-content">
+                            <p>We identified the following keywords:</p>
+                        </div>
+                    </div><br/><br/>
+                    <div id="any-form-hidden-content">
+                        <p>The recommended courses are filtered in the table.</p>
+                        <span>If you like what you see let us add them to your profile!</span>
+                        <button className={ai.button}>Add</button>
+                    </div>
+                </div>
 
-                        {getFormattedOrar(optionale)}
-                    </tbody>
-                </table>
+                <div id={ai.aiRight}>
+                    <p>{username}</p>
+                    <img src={profilePic} />
+                    <p>{about}</p>
+
+                    <table className={ai.table}>
+                        <tbody>
+                            <tr>
+                                <th>Optionale Sugerate</th>
+                            </tr>
+
+                            {getFormattedOrar(optionale)}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
